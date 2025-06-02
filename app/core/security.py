12bbae2495f4c37b53644768a83a9cb1416f21f3
paulebil <ebilpaul06@gmail.com>
2025-06-02 15:46:12 +0300

    created a get current user dependency

--- app/core/security.py
+++ app/core/security.py
@@ -8,3 +8,18 @@ def verify_password(plain_password, hashed_password):
 
 def hash_password(password):
     return pwd_context.hash(password)
+
+
+from fastapi import Depends, Header, HTTPException
+from app.repository.user import UserRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database.database import get_session
+from app.models.user import User
+
+
+async def get_current_user(user_id: int = Header(...), session: AsyncSession = Depends(get_session)) -> User:
+    user_repository = UserRepository(session)
+    user = await user_repository.get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
